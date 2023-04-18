12da809b2a21122fcf7dbdde74c0914b6234753f
Lair Martes <lairjunior@gmail.com>
2023-04-18 16:24:57 -0500

    Fix typo in aws attribute name

--- terraform/05-ec2-instances/ec2-instances.tf
+++ terraform/05-ec2-instances/ec2-instances.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "http_server" {
     ami = "ami-069aabeee6f53e7bf"
     key_name = "default-ec2"
     instance_type = "t2.micro"
-    vcp_security_group_ids = [aws_security_group.http_server_sg.id]
+    vpc_security_group_ids = [aws_security_group.http_server_sg.id]
     subnet_id = "subnet-00455aaafc456df5a"
 }
