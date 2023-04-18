0c9dca35435d11db1b8e760083fe47079acd5adc
Lair Martes <lairjunior@gmail.com>
2023-04-18 17:52:05 -0500

    Use data from aws_subnets

--- terraform/05-ec2-instances/ec2-instances.tf
+++ terraform/05-ec2-instances/ec2-instances.tf
@@ -1,9 +1,16 @@
+data "aws_subnets" "default_subnets" {
+    filter {
+        name = "vpc-id"
+        values = [aws_default_vpc.default.id]
+    }
+}
+
 resource "aws_instance" "http_server" {
     ami = "ami-069aabeee6f53e7bf"
     key_name = "default-ec2"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-    subnet_id = "subnet-00455aaafc456df5a"
+    subnet_id = data.aws_subnets.default_subnets.ids[0]
 
     connection {
 
