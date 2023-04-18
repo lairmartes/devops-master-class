f1ac34f1aff4b94694b856d28473c3af2e12fbf3
Lair Martes <lairjunior@gmail.com>
2023-04-18 10:50:31 -0500

    Create http server security groups

---
+++ terraform/05-ec2-instances/ec2instances.tf
@@ -0,0 +1,32 @@
+# security group
+
+resource "aws_security_group" "http_server_sg" {
+    name = "http_server_sg"
+    vpc_id = "vpc-087fe822880300f80"
+
+    ingress {  
+    
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {  
+    
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    egress {
+
+        from_port = 0 # all ports
+        to_port = 0
+        protocol = -1 # all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
