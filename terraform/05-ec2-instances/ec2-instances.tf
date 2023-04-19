3293bf962c9e897d517eb94d54c904b8b3908e30
Lair Martes <lairjunior@gmail.com>
2023-04-18 19:10:17 -0500

    use data provider to define correct ami id + refactor - file for data providers

--- terraform/05-ec2-instances/ec2-instances.tf
+++ terraform/05-ec2-instances/ec2-instances.tf
@@ -1,30 +1,5 @@
-data "aws_subnets" "default_subnets" {
-    filter {
-        name = "vpc-id"
-        values = [aws_default_vpc.default.id]
-    }
-}
-
-data "aws_ami_ids" "aws_linux_2_latest_ids" {
-    owners      = ["amazon"]
-}
-
-data "aws_ami" "aws_linux_2_latest" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-kernel-5.10-hvm-*"]
-    }
-
-    filter {
-        name = "architecture"
-        values = ["x86_64"]
-    }
-}
-
 resource "aws_instance" "http_server" {
-    ami = "ami-069aabeee6f53e7bf"
+    ami = data.aws_ami.aws_linux_2_latest.id
     key_name = "default-ec2"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.http_server_sg.id]
