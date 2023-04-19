7b1d209c7dc0f357d89bd8580eba38c9b89390d9
Lair Martes <lairjunior@gmail.com>
2023-04-18 19:05:53 -0500

    Include more filters to fetch correct OS

--- terraform/05-ec2-instances/ec2-instances.tf
+++ terraform/05-ec2-instances/ec2-instances.tf
@@ -14,7 +14,12 @@ data "aws_ami" "aws_linux_2_latest" {
     owners = ["amazon"]
     filter {
         name = "name"
-        values = ["amzn2-ami-hvm-*"]
+        values = ["amzn2-ami-kernel-5.10-hvm-*"]
+    }
+
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
     }
 }
 
