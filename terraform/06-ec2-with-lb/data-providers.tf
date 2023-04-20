b1f70151de07d8d5e5aa66f0fe10e17d6326a655
Lair Martes <lairjunior@gmail.com>
2023-04-19 19:27:04 -0500

    Fix subnet vpc id

--- terraform/06-ec2-with-lb/data-providers.tf
+++ terraform/06-ec2-with-lb/data-providers.tf
@@ -1,8 +1,6 @@
 data "aws_subnet_ids" "default_subnets" {
-    filter {
-        name = "vpc-id"
-        values = [aws_default_vpc.default.id]
-    }
+
+    vpc_id = aws_default_vpc.default.id
 }
 
 data "aws_ami_ids" "aws_linux_2_latest_ids" {
