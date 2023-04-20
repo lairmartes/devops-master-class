493367bc487e4edcc4aab94e6a08749c84d2c144
Lair Martes <lairjunior@gmail.com>
2023-04-19 19:30:36 -0500

    don´t specify http servers fields in output (we don´t now how to show each one)

--- terraform/06-ec2-with-lb/outputs.tf
+++ terraform/06-ec2-with-lb/outputs.tf
@@ -2,10 +2,6 @@ output "aws_security_group_http_server_details" {
     value = aws_security_group.http_server_sg.arn
 }
 
-output "aws_instance_http_server_details" {
-    value = aws_instance.http_servers.*.arn
-}
-
-output "http_servers_public_dns" {
-    value = aws_instance.http_servers.*.public_dns
+output "aws_instance_http_servers" {
+    value = aws_instance.http_servers
 }
