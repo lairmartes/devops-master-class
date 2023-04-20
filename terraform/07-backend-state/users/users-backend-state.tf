cf96b27ead65648310c1d8a898584f6285462fe1
Lair Martes <lairjunior@gmail.com>
2023-04-20 11:48:29 -0500

    move state to folders

--- terraform/07-backend-state/users/users-backend-state.tf
+++ terraform/07-backend-state/users/users-backend-state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "dev-applications-backend-state-in28minutes-lair"
-        key = "07-backend-state-users-dev"
+        key = "07-backend-state/users/backend-state"
         region = "us-east-1"
         dynamodb_table = "dev_application_locks"
         encrypt = true
