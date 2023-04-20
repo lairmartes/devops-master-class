41e70a2fe42327d7d5a3eb12a8d1040808686a4a
Lair Martes <lairjunior@gmail.com>
2023-04-20 11:09:34 -0500

    Create project to keep tf state

---
+++ terraform/07-backend-state/backend-state/s3-backend.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "enterprise_backend_state" {
+    bucket = "dev/applications/backend-state/in28minutes-lair"
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+}
+
+resource "aws_s3_bucket_versioning" "backend_state_versioning" {
+    bucket = aws_s3_bucket.enterprise_backend_state.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend_state_encryption" {
+
+    bucket = aws_s3_bucket.enterprise_backend_state.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            
+            sse_algorithm = "AES256"
+        }
+    }
+}
